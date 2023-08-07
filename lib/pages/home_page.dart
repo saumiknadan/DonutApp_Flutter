c05ee2070b0eda9422f8593725445e364edf9ca4
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-08-08 02:42:43 +0600

    appbar icon placement & asset add

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 24),
+          child: Icon(
+            Icons.menu,
+            color: Colors.grey,
+            size: 36,
+          ),
+        ),
+
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(
+              Icons.person,
+              color: Colors.grey,
+              size: 36,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
